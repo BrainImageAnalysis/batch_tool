c72059eb8b3afcc60ce6e75154aedbb4fb8ab130
Matthias Schlachter <matthias.schlachter@riken.jp>
2023-11-07 17:35:15 +0900

    update run_samples.sh

--- samples/run_samples.sh
+++ samples/run_samples.sh
@@ -14,10 +14,13 @@ SCRIPT=0
 eval pybatch.py --script ~/python/batch_tool/samples/test_script.py --infiles b1/1 b1/2 b2/3 b3/4 b4/5 b4/6 b5/7 b5/8 b5/9 b5/10  --verbose ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
 
 ((SCRIPT++))
+eval pybatch.py --script ~/python/batch_tool/samples/test_script.py --infiles b1/1 b1/2 b2/3 b3/4 b4/5 b4/6 b5/7 b5/8 b5/9 b5/10  --verbose --dry-run ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
+
+((SCRIPT++))
 eval pybatch.py --script ~/python/batch_tool/samples/test_script.py --infiles 1 --parameter thres=0.75 -p result=result.nii.gz --dry-run ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
 
 ((SCRIPT++))
-eval pybatch.py --no-shadow --script ~/python/batch_tool/samples/test_script.py --infiles 1 --parameter thres=0.75 -p result=result.nii.gz --dry-run ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
+eval pybatch.py --no-shadow --script ~/python/batch_tool/samples/test_script.py --infiles 1 --parameter thres=0.75 -p result=result.nii.gz ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
 
 ((SCRIPT++))
 eval pybatch.py --max_workers 4 --script ~/python/batch_tool/samples/test_script2.py --infiles 1 2 3 4 5 6 7 8 9 10  --verbose ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
