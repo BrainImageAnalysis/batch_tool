cd62af359c4bb765d1f340d7599b1350e6b5e0c8
Matthias Schlachter <matthias.schlachter@riken.jp>
2023-11-07 16:40:54 +0900

    update run_samples.sh

--- samples/run_samples.sh
+++ samples/run_samples.sh
@@ -8,21 +8,46 @@ if [ "$1" == '-q' ] || [ "$1" == '-quiet' ];then
 fi
 
 CNT=0
+SCRIPT=0
 
-eval pybatch.py --script ~/python/batch_tool/samples/test_script.py --infiles 1 2 3 4 5 6 7 8 9 10  --verbose ${OUT} || echo "script failed: $((CNT++))"
+((SCRIPT++))
+eval pybatch.py --script ~/python/batch_tool/samples/test_script.py --infiles b1/1 b1/2 b2/3 b3/4 b4/5 b4/6 b5/7 b5/8 b5/9 b5/10  --verbose ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
 
-eval pybatch.py --script ~/python/batch_tool/samples/test_script.py --infiles 1 --parameter thres=0.75 -p result=result.nii.gz --dry-run ${OUT} || echo "script failed: $((CNT++))"
+((SCRIPT++))
+eval pybatch.py --script ~/python/batch_tool/samples/test_script.py --infiles 1 --parameter thres=0.75 -p result=result.nii.gz --dry-run ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
 
-eval pybatch.py --max_workers 4 --script ~/python/batch_tool/samples/test_script2.py --infiles 1 2 3 4 5 6 7 8 9 10  --verbose ${OUT} || echo "script failed: $((CNT++))"
+((SCRIPT++))
+eval pybatch.py --no-shadow --script ~/python/batch_tool/samples/test_script.py --infiles 1 --parameter thres=0.75 -p result=result.nii.gz --dry-run ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
 
-eval pybatch.py --max_workers 10 --script ~/python/batch_tool/samples/test_script3.py --infiles 1 2 3 4 5 6 7 8 9 10  --verbose ${OUT} || echo "script failed: $((CNT++))"
+((SCRIPT++))
+eval pybatch.py --max_workers 4 --script ~/python/batch_tool/samples/test_script2.py --infiles 1 2 3 4 5 6 7 8 9 10  --verbose ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
 
-eval conda run -n pytorch pybatch.py --script ~/python/batch_tool/samples/test_script4.py --infiles ~/python/batch_tool/**/*.py  --verbose ${OUT} || echo "script failed: $((CNT++))"
+((SCRIPT++))
+eval pybatch.py --max_workers 10 --script ~/python/batch_tool/samples/test_script3.py --infiles 1 2 3 4 5 6 7 8 9 10  --verbose ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
 
-eval pybatch.py --max_workers 10 --script ~/python/batch_tool/samples/test_script5.py --infiles 1 2 3 4 5 6 7 8 9 10  --verbose ${OUT} || echo "script failed: $((CNT++))"
+((SCRIPT++))
+eval conda run -n pytorch2 pybatch.py --script ~/python/batch_tool/samples/test_script4.py --infiles ~/python/batch_tool/**/*.py  --verbose ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
 
-eval pybatch.py --max_workers 10 --script ~/python/batch_tool/samples/test_script6.py --infiles 1 2 3 4 5 6 7 8 9 10  --verbose ${OUT} || echo "script failed: $((CNT++))"
+((SCRIPT++))
+eval pybatch.py --max_workers 10 --script ~/python/batch_tool/samples/test_script5.py --infiles 1 2 3 4 5 6 7 8 9 10  --verbose ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
+
+((SCRIPT++))
+eval pybatch.py --max_workers 10 --script ~/python/batch_tool/samples/test_script6.py --infiles 1 2 3 4 5 6 7 8 9 10  --verbose ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
 
 pushd "${SCRIPT_PATH}/.." >/dev/null || exit
-eval pybatch.py "@${SCRIPT_PATH}/cmdline.txt" --dry-run ${OUT} || echo "script failed: $((CNT++))"
+
+((SCRIPT++))
+eval pybatch.py "@${SCRIPT_PATH}/cmdline.txt" --dry-run ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
+
+((SCRIPT++))
+eval conda run -n pytorch2 pybatch.py "@${SCRIPT_PATH}/cmdline7.txt" ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
+
+((SCRIPT++))
+eval conda run -n pytorch2 pybatch.py "@${SCRIPT_PATH}/cmdline7-2.txt" ${OUT} || echo "script ${SCRIPT} failed: $((CNT++))"
+
+#############################
+echo ""
+echo "----------------------"
+echo "scripts failed / run: $CNT / $SCRIPT"
+
 popd  >/dev/null || exit
